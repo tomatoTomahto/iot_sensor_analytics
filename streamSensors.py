aba44810f95ca5393e6b237dc9ef8f6039f1af55
Cloud User <ec2-user@ip-10-0-0-57.us-west-2.compute.internal>
2016-12-30 15:49:11 -0500

    kafka and streaming updates

--- streamSensors.py
+++ streamSensors.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession, Row, SQLContext
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+# SparkSession singleton generator needed to operate on Dataframes
 def getSparkSessionInstance(sparkConf):
     if ('sparkSessionSingletonInstance' not in globals()):
         globals()['sparkSessionSingletonInstance'] = SparkSession\
@@ -12,6 +13,7 @@ def getSparkSessionInstance(sparkConf):
             .getOrCreate()
     return globals()['sparkSessionSingletonInstance']
 
+# Initialize SparkSession variable and Spark/Streaming/SQL Contexts
 spark = SparkSession\
     .builder\
     .appName("Streaming Historian Data")\
@@ -21,53 +23,59 @@ sc = spark.sparkContext
 ssc = StreamingContext(sc, 10)
 sqc = SQLContext(sc, spark)
 
+# Turn off INFO/WARN logs
 sc.setLogLevel('FATAL')
 
-lines = ssc.socketTextStream('10.0.0.57', 9999)
-sensorDS = lines.map(lambda line: line.split(","))\
-    .map(lambda (tag_id, value): (int(tag_id), int(value)))\
-    .window(20,10)
-
-fields = [StructField('tag_id', IntegerType()), StructField('value',IntegerType())]
-schema = StructType(fields)
-
+# Read in Tag ID/Entity/Well mappings from Kudu to join with sensor data
 tag_mappings = sqc.read.format('org.apache.kudu.spark.kudu')\
     .option('kudu.master','10.0.0.48')\
     .option('kudu.table','tag_mappings')\
     .load()
 
+# Persist in memory for fast lookup
 tag_mappings.persist()
-tag_mappings.registerTempTable('tag_mappings')
 tag_mappings.collect()
 tag_mappings.show()
+
+# Read in sensor data from Kafka at 10 second intervals with a 20 second rolling window
+kafkaStream = KafkaUtils.createDirectStream(ssc, [historian], {"metadata.broker.list": '10.0.0.134:9092'})
+sensorDS = kafkaStream.map(lambda x: x[1].split(","))\
+    .window(20,10)
 
 def process(time, rdd):
     print("========= %s =========" % str(time))
     try:
         spark = getSparkSessionInstance(rdd.context.getConf())
 
-        sensorDF = spark.createDataFrame(rdd, schema)
-        sensorDF.registerTempTable('sensor_data')
-#        sensorDF.show()
+        # Read in the raw data and convert timestamp to a string integer
+        rawSensor = spark.createDataFrame(rdd)\
+            .toDF('record_time', 'tag_id', 'value')\
+            .withColumn('record_time', regexp_replace('record_time', '[-: ]+', ''))
 
-        tempSensor = sensorDF.join(tag_mappings, 'tag_id', 'inner')
-        tempSensor.registerTempTable('df1')
-#        tempSensor.show()
+        # Cast all columns to the right data types (record_time:bigint, tag_id:int, value:float)
+        castSensor = rawSensor.select(rawData.record_time.cast('bigint'), 
+                                      rawData.tag_id.cast('integer'), 
+                                      rawData.value.cast('float'))
 
-        fullSensor = tempSensor.join(tag_mappings, ['tag_id','tag_entity'], 'right_outer')\
-            .filter(
-            .select(tag_mappings.tag_entity, tag_mappings.tag_description, tempSensor.value)
+        # Join the sensor data with tag ID mappings
+        taggedSensor = rawSensor.join(tag_mappings, 'tag_id', 'inner')
 
-#        fullSensor = sqc.sql('select tm.tag_id, tm.tag_entity, tm.tag_description, df1.value\
-#                              from df1 right join tag_mappings tm\
-#                                on (df1.tag_id == tm.tag_id and df1.tag_entity = tm.tag_entity)')
-#        fullSensor.show()
+        # Rejoin the data with tag entity and well mappings
+        fullSensor = tempSensor.join(tag_mappings, ['tag_id','well','tag_entity'], 'right_outer')
 
-        finalSensor = fullSensor.selectgroupBy('tag_entity')\
-            .pivot('tag_description')\
-            .agg(sum('value'))
+        # Pivot the data to show 1 column for each tag entity
+        finalSensor = fullSensor.groupBy('measurement_time','well')\
+            .pivot('tag_entity')\
+            .agg(sum('value'))\
+            .filter('record_time is not null')
         finalSensor.show()
 
+        # Write to KUDU table
+        finalSensor.write.format('org.apache.kudu.spark.kudu')\
+            .option("kudu.master", '10.0.0.48')\
+            .option("kudu.table", "sensor_measurements")\
+            .mode("append")\
+            .save()
     except:
         pass
 
